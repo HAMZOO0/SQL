9cb7780495f4c446310123e1182215b2932419fb
Hamza Khan <98114762+HAMZOO0@users.noreply.github.com>
2024-02-20 22:51:45 +0500

    Create learning.sql

---
+++ learning.sql
@@ -0,0 +1,7 @@
+-- to create a  database use == create database (name )
+-- create database tiba1; 
+-- DROP DATABASE tiba1;
+-- delete the data base by using drop database (name )
+
+CREATE DATABASE NUST;
+
