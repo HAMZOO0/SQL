4347ba2829ac7133d157dea414749e5d81f7b14b
Hamza Khan <98114762+HAMZOO0@users.noreply.github.com>
2024-02-21 22:36:22 +0500

    Create learning_2.sql
    How to :
    create Tables 
    Add columns 
    Add data in table 
    Print Table

---
+++ learning_2.sql
@@ -0,0 +1,22 @@
+create  database school;
+
+ use school ;
+ -- use - use to select database on which we want to work on  
+ 
+CREATE TABLE student(
+  id int primary key ,
+  name varchar(50) , 
+  age int not null 
+  );
+   -- here we are creating table , which is based on  3 colums 
+   -- id is the name of col , int is datatype , 3rd is constrains 
+   -- name is the name of col , then varchar is datatype which is based on 50 char long 
+   -- 3rd is name age and it is INT and  constatins we add what not null 
+   
+   INSERT INTO student VALUES (1 , 'Ayaan', 19);
+   INSERT INTO student VALUES (2 , 'Iqra', 21);
+   INSERT INTO student VALUES(3, 'Hamza', 20);
+
+   Select * from student;
+   -- above syntex is use for print the table 
+   
