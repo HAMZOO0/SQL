4cfef55762b8a275e4f6df5b63ba1d2c91d49708
Hamza Khan <98114762+HAMZOO0@users.noreply.github.com>
2024-02-29 20:49:56 -0800

    Update where + select.sql

--- where + select.sql
+++ where + select.sql
@@ -7,3 +7,11 @@ select name from student ;
 select * from student ; 
 -- it will print whole table 
 
+select *from student where  city= "Karachi";
+-- here we add where clause to add condition 
+
+select *from student where  city= "Karachi" AND id = 3;
+-- Here we add multiple condtion 
+
+select *from student where  id+5 =  10 ;
+-- here we add diffent mathematical opeartion and condition 
