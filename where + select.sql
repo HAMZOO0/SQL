4d7573e0627805fb26c622587b6a47cbec9ade4c
Hamza Khan <98114762+HAMZOO0@users.noreply.github.com>
2024-02-29 20:34:06 -0800

    Create where + select.sql

---
+++ where + select.sql
@@ -0,0 +1,9 @@
+select distinct name from student ; 
+-- here we select uniqe data  or print 
+
+select name from student ; 
+-- here we print just simple 1 colm or how much we want 
+
+select * from student ; 
+-- it will print whole table 
+
